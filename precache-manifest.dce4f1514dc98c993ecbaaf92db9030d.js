1c49d06069d4beffa9c00c7583b72cfef08f1c22
Osher Ben David <Osherb@cymulate.com>
2020-04-18 12:35:08 +0300

    Updates

---
+++ precache-manifest.dce4f1514dc98c993ecbaaf92db9030d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab3bc6cdeb18cfbfc48d3c09ad09fb2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "37dd7dd040426eff562a",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "fbf56bb72df0870f79c8",
+    "url": "/robofriends/static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "37dd7dd040426eff562a",
+    "url": "/robofriends/static/js/2.594487ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.594487ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf56bb72df0870f79c8",
+    "url": "/robofriends/static/js/main.ff59c5c0.chunk.js"
+  },
+  {
+    "revision": "de33b1b324b36ed63b52",
+    "url": "/robofriends/static/js/runtime-main.ecae73b6.js"
+  }
+]);
